4c549a075cd20f9f959ccfad8fe44d4430dfb039
KID <134874155+kid-Baasan@users.noreply.github.com>
2023-05-31 23:35:16 +0600

    Add files via upload

---
+++ gshjajgv.js
@@ -0,0 +1,37 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var score=0;
+
+var gameOver, restart;
+
+
+
+function preload(){
+  trex_running =   loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+
+  
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
+}
+
+function
